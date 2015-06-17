fc1b6df5ddbb6ae4d8de3b7569511bb444c61bbc
alexanderivanovdev <alexander.ivanov.dev@gmail.com>
2015-06-17 18:49:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,27 +7,41 @@ app.controller('doCtrl', function($scope, $http) {
         type: 'get',
         p: 'JSONP'
     }, {
+        type: 'get',
+        p: 'TEXT'
+    }, {
         type: 'post',
         p: 'JSON'
     }, {
         type: 'post',
         p: 'JSONP'
     }, {
+        type: 'post',
+        p: 'TEXT'
+    }, {
         type: 'jsonp',
         p: 'JSONP'
     }, {
         type: 'jsonp',
         p: 'JSON'
+    }, {
+        type: 'jsonp',
+        p: 'TEXT'
     }];
 });
 app.controller('reqCtrl', function($scope, $http) {
     $scope.url = 'https://script.google.com/macros/s/AKfycbyIb3ZdA_SZY3UyVpNZSAdGsFiSZYuIzoAMwk-VK50_3YNaqlw/exec?' + $scope.req.p + '=' + $scope.req.type + '&callback=JSON_CALLBACK';
-    $http({method: $scope.req.type, url : $scope.url}).success(function(data) {
-    	$scope.class = 'success';
-    	$scope.data = JSON.stringify(data);
-    }).error(function(err){
-    	$scope.class = 'err';
-    	$scope.data = JSON.stringify(err);
-
-    });
+    $http({
+        method: $scope.req.type,
+        url: $scope.url,
+        data: {}
+    }).then(function(data) {
+        $scope.class = 'success';
+        delete data.config.url;
+        $scope.data = data;
+    }, function(data) {
+        $scope.class = 'danger';
+        delete data.config.url;
+        $scope.data = data;
+    })
 });
